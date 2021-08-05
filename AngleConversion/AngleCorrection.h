0a6ed5db22902b5174bfeda3c0dd3f5437d71b7c
Reboot <43562820+imtiazsyed006@users.noreply.github.com>
2021-08-06 01:44:41 +0500

    Add files via upload
    This program converts an angle from 0-360 range to negative infinity to positive infinity.

---
+++ AngleConversion/AngleCorrection.h
@@ -0,0 +1,22 @@
+#ifndef ANGLECORRECTION_H_
+#define ANGLECORRECTION_H_
+
+
+double fcn(double u,double u1, double s1, double n1, double ncount1,double rem1);
+
+double fcny(void);
+
+double fcns(void);
+
+double fcnn(void);
+
+double fcnup(void);
+
+double fcnrem(void);
+
+double fcnncount(void);
+
+#endif
+//=====================================================================================================
+// End of file
+//=====================================================================================================
