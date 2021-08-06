9d4b35d6697d715f8b3c0c860b3c380f207e1730
Reboot <43562820+imtiazsyed006@users.noreply.github.com>
2021-08-06 13:03:49 +0500

    Add files via upload

---
+++ MadgwickAHRS/MadgwickAHRS.h
@@ -0,0 +1,23 @@
+#ifndef MYFN
+#define MYFN
+
+MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
+intialize();
+float one(void);
+
+float two(void);
+
+float three(void);
+
+float four(void);
+
+float pitch(void);
+
+float roll(void);
+
+float yaw(void);
+
+#endif
+//=====================================================================================================
+// End of file
+//=====================================================================================================
