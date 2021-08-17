784856c032a043610577b2811cf106c70ced5a34
Reboot <43562820+imtiazsyed006@users.noreply.github.com>
2021-08-18 01:32:23 +0500

    Update AngleCorrection.c

--- AngleConversion/AngleCorrection.c
+++ AngleConversion/AngleCorrection.c
@@ -2,86 +2,74 @@
 #include "AngleCorrection.h"
 #include "math.h"
 
+double b = 0;
+double s_t = 0;
+double n_t = 0;
+double rem_t = 0;
+double ncount_t = 0;
+double error = 0;
+
+double y;
+double s;
+double n;
+double up;
+double rem;
+double ncount;
+
+dataStory datastory;
 
 
-
-double store[6] = {0,0,0,0,0,0};
-
-double fcny(void)
+// double u,double datastory.u1, double datastory.s1, double datastory.n1, double datastory.ncount1,double rem
+void fcn(void)
 {
-    return store[0];
-}
-double fcns(void)
-{
-    return store[1];
-}
-double fcnn(void)
-{
-    return store[2];
-}
-double fcnup(void)
-{
-    return store[3];
-}
-double fcnrem(void)
-{
-    return store[4];
-}
-double fcnncount(void)
-{
-    return store[5];
-}
-
-double fcn(double u,double u1, double s1, double n1, double ncount1,double rem1)
-{
-    double b = u1;
-    double s_t = s1;
-    double n_t = n1;
-    double rem_t = rem1;
-    double ncount_t = ncount1;
-    double error = u1 - u;
+    b = datastory.u1;
+    s_t = datastory.s1;
+    n_t = datastory.n1;
+    rem_t = datastory.rem1;
+    ncount_t = datastory.ncount1;
+    error = datastory.u1 - datastory.u;
     if (error < (-250))
     {
-        if (s1 == 0)
+        if (datastory.s1 == 0)
         {
             s_t = -1;
-            b = u - 360;
+            b = datastory.u - 360;
             rem_t = 0;
         }
-        else if (s1 == (-1))
+        else if (datastory.s1 == (-1))
         {
-            n_t = n1 - 1;
+            n_t = datastory.n1 - 1;
             if (ncount_t > 0)
             {
                 if (n_t <= 0)
                 {
-                    b = u;
+                    b = datastory.u;
                     rem_t = 1;
                 }
                 else if (n_t > 0)
                 {
-                    b = u;
+                    b = datastory.u;
                     rem_t = 0;
                 }
             }
             else 
             {
-                b = u - 360;
+                b = datastory.u - 360;
                 rem_t = 0;
             }
         }
-        else if (s1 == 1)
+        else if (datastory.s1 == 1)
         {
             s_t = -1;
-            if (n1 == 0)
+            if (datastory.n1 == 0)
             {
-               b = u -360;
+               b = datastory.u -360;
                
             }
-            else if (n1 != 0)
+            else if (datastory.n1 != 0)
             {
-                n_t = n1 - 1;
-                b = u;
+                n_t = datastory.n1 - 1;
+                b = datastory.u;
                 if (n_t == 0)
                 {
                     rem_t = 1;
@@ -92,36 +80,36 @@ double fcn(double u,double u1, double s1, double n1, double ncount1,double rem1)
     }
     else if (error > 250)
     {
-        if (s1 == 0)
+        if (datastory.s1 == 0)
         {
             s_t = 1;
-            n_t = n1+1;
-            b = u;
+            n_t = datastory.n1+1;
+            b = datastory.u;
             rem_t = 0;
             
         }
-        else if (s1 == 1)
+        else if (datastory.s1 == 1)
         {
-            n_t = n1 + 1;
-            b = u;
+            n_t = datastory.n1 + 1;
+            b = datastory.u;
             
         }
-        else if (s1 == -1)
+        else if (datastory.s1 == -1)
         {
             s_t = 1;
-            if (n1 == 1)
+            if (datastory.n1 == 1)
             {
-              b = u;  
+              b = datastory.u;  
             }
-            else if (n1 > 0)
+            else if (datastory.n1 > 0)
             {
-                n_t = n1 + 1;
-                b = u;
+                n_t = datastory.n1 + 1;
+                b = datastory.u;
             }
-            else if (n1 < 0 && rem_t == 1)
+            else if (datastory.n1 < 0 && rem_t == 1)
             {
-                n_t = n1 + 1;
-                b = u;
+                n_t = datastory.n1 + 1;
+                b = datastory.u;
             }
         }
     }
@@ -130,16 +118,16 @@ if (s_t == -1)
 {
     if (n_t > 0 || rem_t > 0)
     {
-        b = u;
+        b = datastory.u;
     }
     else 
     {
-        b = u - 360;
+        b = datastory.u - 360;
     }
 }
 else if ( s_t >= 0)
 {
-    b = u;
+    b = datastory.u;
 }
 
 if (n_t > 0)
@@ -147,19 +135,12 @@ if (n_t > 0)
     ncount_t = 1;
 }
 
-double y = b;
-double n = n_t;
-double s = s_t;
-double rem = rem_t;
-double up = u;
-double ncount = ncount_t;
+y = b;
+n = n_t;
+s = s_t;
+rem = rem_t;
+up = datastory.u;
+ncount = ncount_t;
 
-store[0] = y;
-store[1] = s;
-store[2] = n;
-store[3] = up;
-store[4] = rem;
-store[5] = ncount;
-return n;
 }
 
